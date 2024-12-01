f4e46a627b8707b7181a69c2cb23b1370869d316
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-12-01 16:14:39 +0000

    Draft slot model -- in progress

--- volunteer/models.py
+++ volunteer/models.py
@@ -15,5 +15,16 @@ class Beneficiary(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
-# class Slot(model.Model):
-#     beneficiary = models.ForeignKey(Beneficiary, CharField(max_length=150))
+# class Slot(models.Model):
+#     beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE, related_name="slots")
+#     task = models.CharField(max_length="200")
+#     special_reqs = models.CharField(max_length=100, default="")
+#     date = models.DateTimeField(blank=True)
+#     start_time = models.TimeField(blank=True)
+#     end_time = models.TimeField(blank=True)
+#     custom = models.BooleanField(default=True)
+#     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     updated_on = models.DateTimeField(auto_now=True)
+#     reserved_by = model.ForeignKey(Student)
+#     completed_on = models.DateTimeField(blank=True, )
