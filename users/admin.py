f10ae7a093c455c7acf9041c60fd315936137efc
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-12-12 07:29:24 +0000

    Revise Beneficiary and Slot models and migrate to database

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Homeroom, Profile
+# from .models import Homeroom, Profile
 
 # Register your models here.
 # admin.site.register(Homeroom)
-admin.site.register(Profile)
+# admin.site.register(Profile)
 
-@admin.register(Homeroom)
-class HomeroomAdmin(admin.ModelAdmin):
-    list_display = ("homeroom_number", "class_year")
-    ordering = ("homeroom_number",)
+# @admin.register(Homeroom)
+# class HomeroomAdmin(admin.ModelAdmin):
+#     list_display = ("homeroom_number", "class_year")
+#     ordering = ("homeroom_number",)
 
--- users/models.py
+++ users/models.py
@@ -1,34 +1,30 @@
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User, Homeroom
 
-PROFILE_TYPE = ((0, "Student"), (1, "Teacher"))
-CLASS_YEARS = (
-    ("FR", "Freshman"),
-    ("SO", "Sophmore"),
-    ("JR", "Junior"),
-    ("SR", "Senior"),
-    )
-
-
-# Create your models here.
-class Homeroom(models.Model):
-    homeroom_number = models.CharField(max_length=5, unique=True)
-    class_year = models.CharField(max_length=4, choices=CLASS_YEARS, default="FR")
-
-    def _str__(self):
-        return self.room_num
+# PROFILE_TYPE = ((0, "Student"), (1, "Teacher"),(2, "School Admin") )
+# CLASS_YEARS = (
+#     ("FR", "Freshman"),
+#     ("SO", "Sophmore"),
+#     ("JR", "Junior"),
+#     ("SR", "Senior"),
+#     ("UA", "Unassigned")
+#     )
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    alias = models.CharField(max_length=15)
-    homeroom_number = models.ForeignKey(Homeroom, blank=True, null=True, on_delete=models.PROTECT)
-    photo_permission = models.BooleanField(default=False)
-    profile_type = models.IntegerField(choices=PROFILE_TYPE, default=0)
+# # Create your models here.
+# class Homeroom(models.Model):
+#     homeroom_number = models.CharField(max_length=10, unique=True)
+#     class_year = models.CharField(max_length=4, choices=CLASS_YEARS, default="FR")
 
-    def __str__(self):
-        return self.user.username
+#     def _str__(self):
+#         return f"{self.homeroom_number} | Class Year: {self.class_year}"
 
 
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     alias = models.CharField(max_length=15)
+#     homeroom_number = models.ForeignKey(Homeroom, on_delete=models.PROTECT)
+#     profile_type = models.IntegerField(choices=PROFILE_TYPE, default=0)
 
-
+#     def __str__(self):
+#         return f"{self.user.username}"
