2bce90874904e937623d7107a738d9781bb6dad6
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-12-13 20:52:06 +0000

    Unregister all models

--- volunteer/admin.py
+++ volunteer/admin.py
@@ -1,32 +1,35 @@
 from django.contrib import admin
-from .models import Beneficiary, Slot, Homeroom
-from django_summernote.admin import SummernoteModelAdmin
+# from .models import Beneficiary, Slot, Homeroom
+# from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Beneficiary)
-class BeneficiaryAdmin(SummernoteModelAdmin):
+# @admin.register(Beneficiary)
+# class BeneficiaryAdmin(SummernoteModelAdmin):
 
-    """
-    BeneficiaryAdmin inherits from SummernoteModelAdmin
-    in order to include the summernote field
-    enabling formatting the beneficiary description
-    in the admin panel.
-    """
+#     """
+#     BeneficiaryAdmin inherits from SummernoteModelAdmin
+#     in order to include the summernote field
+#     enabling formatting the beneficiary description
+#     in the admin panel.
+#     """
 
-    list_display = ('beneficiary_name', 'slug', 'status')
-    search_fields = ['beneficiary_name']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('beneficiary_name',)}
-    summernote_fields = ('description',)
+#     list_display = ('beneficiary_name', 'slug', 'status')
+#     search_fields = ['beneficiary_name']
+#     list_filter = ('status',)
+#     prepopulated_fields = {'slug': ('beneficiary_name',)}
+#     summernote_fields = ('description',)
 
-    def __str__(self):
-        return f"Beneficiary: {self.beneficiary_name}"
-
-
-@admin.register(Homeroom)
-class HomeroomAdmin(admin.ModelAdmin):
-    list_display = ("homeroom_number", "class_year")
+#     def __str__(self):
+#         return f"Beneficiary: {self.beneficiary_name}"
 
 
-# Register your models here.
-admin.site.register(Slot)
+# @admin.register(Homeroom)
+# class HomeroomAdmin(admin.ModelAdmin):
+#     list_display = ("homeroom_number", "class_year")
+
+
+# # Register your models here.
+# admin.site.register(Slot)
+# admin.site.unregister(Beneficiary)
+# admin.site.unregister(Slot)
+# admin.site.unregister(Homeroom)
--- volunteer/views.py
+++ volunteer/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Beneficiary
+# from django.shortcuts import render
+# from django.views import generic
+# from .models import Beneficiary
 
-Create your views here.
-class BeneficiaryList(generic.ListView):
-    queryset = Beneficiary.objects.filter(status=1)
-    template_name = "volunteer/index.html"
-    # paginate_by = 6
+# Create your views here.
+# class BeneficiaryList(generic.ListView):
+#     queryset = Beneficiary.objects.filter(status=1)
+#     template_name = "volunteer/index.html"
+#     # paginate_by = 6
 
-    # class Meta:
-    #     ordering = ["-updated_on"]
+#     # class Meta:
+#     #     ordering = ["-updated_on"]
