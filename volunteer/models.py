42072babbd2acd560bb6c33cf80746844c570448
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-12-13 20:22:00 +0000

    Change secret key

--- volunteer/models.py
+++ volunteer/models.py
@@ -29,32 +29,32 @@ class Beneficiary(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
 
 
-class Slot(models.Model):
+# class Slot(models.Model):
 
-    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.PROTECT, related_name="slots")
-    task = models.CharField(max_length=200, default="")
-    task_location = models.CharField(max_length=200, default="")
-    date = models.DateTimeField(blank=True, null=True)
-    start_time = models.TimeField(blank=True, null=True)
-    end_time = models.TimeField(blank=True, null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    reserved_by = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, related_name="reservations")
-    completed = models.BooleanField(default=False)
-    credit_minutes_requested = models.IntegerField(default=0)
-    teacher_approved = models.BooleanField(default=False)
-    publish_ok = models.BooleanField(default=False)
+#     beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE, related_name="slots")
+#     task = models.CharField(max_length=200, default="")
+#     task_location = models.CharField(max_length=200, default="")
+#     date = models.DateTimeField(blank=True, null=True)
+#     start_time = models.TimeField(blank=True, null=True)
+#     end_time = models.TimeField(blank=True, null=True)
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     updated_on = models.DateTimeField(auto_now=True)
+#     reserved_by = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, related_name="reservations")
+#     completed = models.BooleanField(default=False)
+#     credit_minutes_requested = models.IntegerField(default=0)
+#     teacher_approved = models.BooleanField(default=False)
+#     publish_ok = models.BooleanField(default=False)
 
 
-class Homeroom(models.Model):
+# class Homeroom(models.Model):
 
-    homeroom_number = models.CharField(max_length=10, primary_key=True)
-    class_year = models.CharField(max_length=10, choices=CLASS_YEARS)
+#     homeroom_number = models.CharField(max_length=10, primary_key=True)
+#     class_year = models.CharField(max_length=10, choices=CLASS_YEARS)
 
-    class Meta:
-        app_label = "volunteer"
-        ordering = ["class_year", "homeroom_number"]
+#     class Meta:
+#         app_label = "volunteer"
+#         ordering = ["class_year", "homeroom_number"]
 
-    def _str__(self):
-        return f"{self.homeroom_number} | Class Year: {self.class_year}"
+#     def _str__(self):
+#         return f"{self.homeroom_number} | Class Year: {self.class_year}"
 
