6b9ccaa1fb91afbdabfe5e89eb075689840e4f29
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-12-16 20:30:47 +0000

    Install whitenoise middleware, set static root variable, specify django version in a django-version file

--- be_a_dear/settings.py
+++ be_a_dear/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-christineec-beadearvolu-ld5kecyr888.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -150,6 +150,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
