4c3bd3f9fc19d0776fa557cfca6f645eaa8bab36
Christine Carter <pxe50zgd@students.codeinstitute.net>
2025-01-05 21:05:56 +0000

    Create users app

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+# class Profile(models.Model):
+#     """
+#     A profile to be able to assign users
+#     to homerooms, distinguish teachers from 
+#     students, and allow students to choose an alias.
+#     The model, together with the homeroom model,
+#     will enable comparison of homeroom
+#     stats regarding volunteer hours.
+#     """
+#     user = models.OneToOneField(User)
+
