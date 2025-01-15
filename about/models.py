b20505dcdf4fff35801a279bade34e428f423155
Christine Carter <pxe50zgd@students.codeinstitute.net>
2025-01-15 16:09:44 +0000

    Change default about image to hearts2 jpg

--- about/models.py
+++ about/models.py
@@ -11,7 +11,7 @@ class About(models.Model):
     """
     title = models.CharField(max_length=50)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default="about_default.jpg")
+    featured_image = CloudinaryField('image', default="hearts2.jpg")
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
